7ebad5fa278e6abe1f26fbee6d9c7f22853edc85
dieKn <dai666k@gmail.com>
2019-10-05 15:19:58 +0900

    first commit

---
+++ SmartUnLock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartUnLock
+//
+//  Created by 加藤大 on 2019/10/05.
+//  Copyright © 2019 加藤大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
